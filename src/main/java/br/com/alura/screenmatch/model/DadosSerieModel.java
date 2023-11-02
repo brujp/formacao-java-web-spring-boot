ab77209465d5570d228ee9c0403813011ce2cc15
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2023-11-01 22:29:46 -0300

    📝 Adicionando novas informações na documentação

--- src/main/java/br/com/alura/screenmatch/model/DadosSerieModel.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerieModel.java
@@ -2,7 +2,7 @@ package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
-//Desserializar (Ler) o json retornado pela api OMBD (https://www.omdbapi.com)
+//Desserializar (Converter Json para um objeto Java) o json retornado pela api OMBD (https://www.omdbapi.com)
 public record DadosSerieModel(
         @JsonAlias("Title") String tituloSerie,
         @JsonAlias("Genre")String generoSerie,
